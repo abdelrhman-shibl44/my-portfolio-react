22ee01ebaf31b61673cead94f481459d3b43b029
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-05-05 22:58:57 +0300

    change images with less resolution

--- src/assests/skillsData/skillsData.js
+++ src/assests/skillsData/skillsData.js
@@ -1,8 +1,8 @@
 
 import JavaScript from "../tech-images/JavaScript-logo.png";
 import jQuery from "../tech-images/jQuery.png";
-import HTML from "../tech-images/Html.png";
-import Css from "../tech-images/css.png";
+import HTMLIcon from "../tech-images/Html.png";
+import CssIcon from "../tech-images/css.png";
 import Sass from "../tech-images/sass.png";
 import Ajax from "../tech-images/ajax.png";
 import reactIcon from "../tech-images/react.png";
@@ -11,16 +11,16 @@ import mysql from "../tech-images/mysql.png";
 import nodeJs from "../tech-images/nodeJs.png";
 import gitGithub from "../tech-images/git&Github.jpeg";
 const skillsData = [
-  { id: 1, name: "HTML",icon: HTML},
-  { id: 2, name: "CSS" ,icon: Css },
+  { id: 1, name: "HTML",icon: HTMLIcon},
+  { id: 2, name: "CSS" ,icon: CssIcon },
   { id: 3, name: "JavaScript",icon:JavaScript },
   { id: 4, name: "jQuery",icon:jQuery },
   { id: 5, name: "Ajax" ,icon:Ajax},
   { id: 6, name: "React" ,icon:reactIcon},
   { id: 7, name: "Bootstrap" ,icon:bootstrap},
   { id: 8, name: "Sass",icon:Sass },
-  { id: 10, name: "Node.js",icon:nodeJs},
-  { id: 11, name: "mySql",icon: mysql},
-  { id: 12, name: "git&github",icon: gitGithub},
+  { id: 9, name: "Node.js",icon:nodeJs},
+  { id: 10, name: "mySql",icon: mysql},
+  { id: 11, name: "git&github",icon: gitGithub},
 ];
 export default skillsData
